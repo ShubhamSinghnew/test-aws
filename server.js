4821829c24da3b2d10cbd4ddc23f38acc13fe4ba
ShubhamSinghnew <124259343+ShubhamSinghnew@users.noreply.github.com>
2025-05-27 11:14:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -64,44 +64,44 @@ const getZohoAccessToken = async () => {
   }
 };
 
-// app.get('/auth/zoho/callback', async (req, res) => {
-//   const { code } = req.query;
-//   console.log('code: ', code)
+app.get('/auth/zoho/callback', async (req, res) => {
+  const { code } = req.query;
+  console.log('code: ', code)
 
-//   if (!code) {
-//     return res.status(400).send('Missing code');
-//   }
+  if (!code) {
+    return res.status(400).send('Missing code');
+  }
 
-//   try {
-//     const response = await axios.post(
-//       'https://accounts.zoho.in/oauth/v2/token',
-//       qs.stringify({
-//         code: code,
-//         client_id: '1000.ZVKBEM29FTWQ28GY4YR8QXYODNT4NI',
-//         client_secret: '5fcad4f500f46a07e10e3b6c4cd925556958b42e6e',
-//         redirect_uri: 'https://test-aws-lz6a.onrender.com/auth/zoho/callback',
-//         grant_type: 'authorization_code',
-//       }),
-//       {
-//         headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//       }
-//     );
+  try {
+    const response = await axios.post(
+      'https://accounts.zoho.in/oauth/v2/token',
+      qs.stringify({
+        code: code,
+        client_id: '1000.ZVKBEM29FTWQ28GY4YR8QXYODNT4NI',
+        client_secret: '5fcad4f500f46a07e10e3b6c4cd925556958b42e6e',
+        redirect_uri: 'https://test-aws-lz6a.onrender.com/auth/zoho/callback',
+        grant_type: 'authorization_code',
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
 
-//     const accessToken = response.data.access_token;
-//     const refreshToken = response.data.refresh_token;
+    const accessToken = response.data.access_token;
+    const refreshToken = response.data.refresh_token;
 
-//     console.log('Access Token:', response.data);
+    console.log('Access Token:', response.data);
 
 
 
-//     res.send('Zoho authorization complete. Tokens received.');
-//   } catch (err) {
-//     console.error('Error:', err.response?.data || err.message);
-//     res.status(500).send('Token exchange failed');
-//   }
-// });
+    res.send('Zoho authorization complete. Tokens received.');
+  } catch (err) {
+    console.error('Error:', err.response?.data || err.message);
+    res.status(500).send('Token exchange failed');
+  }
+});
 
 // app.post('/send_sms', async (req, res) => {
 //   try {
@@ -133,17 +133,6 @@ const getZohoAccessToken = async () => {
 //     });
 //   }
 // });
-
-// Download the helper library from https://www.twilio.com/docs/node/install
-
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
-
-// Client ID
-// 1000.ZVKBEM29FTWQ28GY4YR8QXYODNT4NI
-
-// Client Secret
-// 5fcad4f500f46a07e10e3b6c4cd925556958b42e6e
 
 
 app.post('/from-cliq', async (req, res) => {
@@ -232,7 +221,6 @@ app.get('/to_cliq', (req, res) => {
 });
 
 app.post('/to_cliq', async (req, res) => {
-  console.log(JSON.stringify(req.body))
   const entry = req.body.entry?.[0];
   const changes = entry?.changes?.[0];
   const messages = changes?.value?.messages;
